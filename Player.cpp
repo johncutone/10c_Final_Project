3d52f8302e4058b1479dc4acaff36168cc17eeb9
unknown <johncutone@gmail.com>
2018-06-14 14:45:56 -0700

    Slight cleanup. I believe I can now make correct commits from here on out.

--- Player.cpp
+++ Player.cpp
@@ -2,23 +2,10 @@
 
 Player::Player() //: QGraphicsEllipseItem()
 {
-    //setRect(0,0,5,5);
-    //icon = new QGraphicsEllipseItem();
-    //icon->setRect(0,0,5,5);
-    //icon->brush(Qt::black);
+
 }
 
 Player::~Player()
 {
-    //delete *icon;
+
 }
-
-//QGraphicsEllipseItem *Player::getIcon()
-//{
-//    return this->icon;
-//}
-
-//void Player::moveRight()
-//{
-
-//}
